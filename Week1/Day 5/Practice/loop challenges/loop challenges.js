97b1e024ddce61d509d7c92e9c98c4bb423a4d16
2023Raed <raed.messaoud1993@gmail.com>
2023-01-22 21:19:25 +0100

    loop challenges

---
+++ Week1/Day 5/Practice/loop challenges/loop challenges.js
@@ -0,0 +1,24 @@
+for (var i = 1; i <= 20; i++) {
+    if (i % 2 != 0) {
+        console.log(i) // les valeurs imapirs
+    }
+}
+for (var i = 100; index >= 0; i--) {
+    if (i % 3= 0) {
+        console.log(i)
+    }
+}
+var array = [4, 2.5, 1, -0.5, -2, -3.5]
+for (var i = 0; i < array.lengt; i++) {
+    console.log(array[i])
+}
+var sum = 0
+for (var i = 0; index <= 100; i++) {
+    sum = sum + i
+}
+console.log(sum)
+var product = 1
+for (var i = 1; i <= 12; i++) {
+    product = product * i
+}
+console.log(product)
