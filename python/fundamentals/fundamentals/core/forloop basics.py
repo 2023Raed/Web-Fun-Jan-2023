4172da663ca5092c89ba2aa314167904039f73c0
2023Raed <raed.messaoud1993@gmail.com>
2023-01-30 20:09:37 +0100

    forloopbasics

---
+++ python/fundamentals/fundamentals/core/forloop basics.py
@@ -0,0 +1,27 @@
+for i in range(0,151):
+    print(i)
+
+for i in range(5,1001):
+    if(i%5==0):
+        print(i)
+for i in range(1,101):
+    if i %10==0:
+        print("coding dojo")
+    elif i%5==0:
+        print("coding")
+    else:
+        print(i)    
+a=0
+for i in range (0,500001):
+     if(i%2!=0):
+       a=a+i 
+print(a) 
+for i in range(2018,0,-4):
+    print(i)
+lownum=5
+highnum=101
+mult=5
+for i in range (lownum ,highnum):
+   if(i%mult==0):
+    print(i)
+
