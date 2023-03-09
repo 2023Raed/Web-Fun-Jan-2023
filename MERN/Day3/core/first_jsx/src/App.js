27064bfef50ef367a1889e5b0a0888e6f9ec2536
2023Raed <raed.messaoud1993@gmail.com>
2023-03-09 13:17:35 +0100

    First_jsx

---
+++ MERN/Day3/core/first_jsx/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Hello Dojo</h1>
+    <h1>Things i need to:</h1>
+    <ul>
+      <ol>*Learn React</ol>
+      <ol>*Climb Everest</ol>
+      <ol>*Run a marathon</ol>
+      <ol>*Feed the dogs</ol>
+    </ul>
+    </div>
+  );
+}
+
+export default App;
