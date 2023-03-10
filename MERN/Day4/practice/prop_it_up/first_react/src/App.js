a16ae0d4edb66a8c8c24ef92a29517f9e4b5973c
2023Raed <raed.messaoud1993@gmail.com>
2023-03-10 08:59:26 +0100

    prop_it_up

---
+++ MERN/Day4/practice/prop_it_up/first_react/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> <strong>Doe,Jane</strong></h1>
+      <h3>Age:45</h3>
+      <h3>Hair color:black</h3>
+      <h1> <strong>Smith,john</strong></h1>
+      <h3>Age:88</h3>
+      <h3>Hair color:brown</h3>
+      <h1> <strong>Fillmord,Millard</strong></h1>
+      <h3>Age:50</h3>
+      <h3>Hair color:brown</h3>
+      <h1> <strong>Smith,Maria</strong></h1>
+      <h3>Age:62</h3>
+      <h3>Hair color:brown</h3>
+    </div>
+  );
+}
+
+export default App;
